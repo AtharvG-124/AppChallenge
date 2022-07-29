0ab884417220b1aeb536773b9f6a45d8064f2267
AtharvG-124 <atharv.gupta124@gmail.com>
2022-07-28 19:52:34 -0700

    updated with more bundle files

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: ['./src/applicants.js'],
+    entry: {
+        signup: './src/signup.js',
+        login: './src/login.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist/bundles'),
+        filename: '[name].bundle.js'
     },
     watch: true
 };
