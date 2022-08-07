b4566563e21468b75a18ffd38302da32f4d202f2
amomack123 <96466242+amomack123@users.noreply.github.com>
2022-08-07 14:58:03 -0700

    Update login.js

--- src/login.js
+++ src/login.js
@@ -43,3 +43,14 @@ signInWithEmailAndPassword(auth, email, password)
     const errorCode = error.code;
     const errorMessage = error.message;
   });
+const user = auth.currentUser;
+
+if (user !== null) {
+  user.providerData.forEach((profile) => {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+}
